57b7a5192833dc826f9aac25c64d993de24961b7
JANG <kennethjang34@gmail.com>
2022-05-12 13:40:22 -0700

    ChatConsumer implementation completed(not tested), frontend portion of the app created. WebSocketServer implemented

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import WebsocketConsumer
 from chat.api.views import get_chat_rooms
 from chat.api.serializers import *
 from .models import Message, Chat
+import datetime
+from django.shortcuts import get_object_or_404
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -10,9 +12,13 @@ class ChatConsumer(WebsocketConsumer):
     def message_to_json(message):
         return json.dumps(message)
 
-    def get_chat_room(self):
-        Chat.objects.get_or_create()
-        pass
+    # ChatConsumer assumes the chat room has already been created
+    @staticmethod
+    def get_chat_room(room_name):
+        # Chat.objects.get_or_create(
+        #     participants=self., defaults={"messages": [], "timestamp": datetime()}
+        # )
+        return get_object_or_404(Chat, room_name)
 
     # messages: queryset of Message model instances
     @staticmethod
@@ -26,7 +32,7 @@ class ChatConsumer(WebsocketConsumer):
 
     def handle_previous_message_request(self, data):
         # chat_room: must be an instance of Chat model
-        chat_room = data.get_chat_room()
+        chat_room = ChatConsumer.get_chat_room(self.room_name)
         messages = chat_room.messages
         json_messages = ChatConsumer.messages_to_json(messages)
         self.send_previous_messages(json_messages)
@@ -36,8 +42,9 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = "chat_%s" % self.room_name
         # Join room group
         self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        chat_room = self.get_chat_room()
+        chat_room = ChatConsumer.get_chat_room(self.room_name)
         await self.accept()
+        self.send_previous_messages(chat_room.messages)
 
     async def disconnect(self, close_code):
         self.channel_layer.group_discard(self.room_group_name, self.channel_name)

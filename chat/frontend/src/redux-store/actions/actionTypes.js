389ca6ed1ca741b17aecc926b6febe21c788f6b3
JANG <kennethjang34@gmail.com>
2022-05-12 23:31:08 -0700

    chat-frontend directory changed to frontend and moved into chat directory

---
+++ chat/frontend/src/redux-store/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_IN_PROGRESS = "AUTH_SUCCESS";
+export const LOGOUT = "AUTH_SUCCESS";
+
+export const ADD_MESSAGE = "ADD_MESSAGE";
+export const LOAD_MESSAGE = "LOAD_MESSAGE";

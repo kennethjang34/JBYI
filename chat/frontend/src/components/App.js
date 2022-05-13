bd3c8327c51b509e40d956c4dee1e21c1da3e020
JANG <kennethjang34@gmail.com>
2022-05-13 00:39:42 -0700

    redux partially implemented for auth.

--- chat/frontend/src/components/App.js
+++ chat/frontend/src/components/App.js
@@ -3,38 +3,30 @@ import webSocketServer from "../websocket";
 import { connect } from "react-redux";
 import * as authActions from "../redux-store/actions/authActions";
 import * as messageActions from "../redux-store/actions/messageActions";
-import Chat from "./Chat";
+import ChatRoom from "./ChatRoom";
+import ChatApp from "./ChatApp";
 import SidePanel from "./SidePanel";
 class App extends React.Component {
     constructor(props) {
         super(props);
-        is_authenticated();
-        // webSocketServer.setMessageHandlers(
-        //     this.props.loadMessages,
-        //     this.props.addMessage
-        // );
+        checkAuth();
     }
 
     render() {
-        return (
-            <div>
-                <SidePanel />
-                <Chat />
-            </div>
-        );
+        return <ChatApp />;
     }
 }
 const mapStateToProps = (state) => {
     return {
-        currentUser: state.auth.currentUser.username,
+        // currentUser: state.auth.currentUser.username,
         //state.chat.chats: list of chats. Each chat contains messges belonging to that chat room
-        chats: state.chat.chats,
+        // chats: state.chat.chats,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        is_authenticated: () => dispatch(actions.checkAuth),
+        checkAuth: () => dispatch(authActions.checkAuthAction),
     };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- chat/frontend/src/redux-store/actions/authActions.js
+++ chat/frontend/src/redux-store/actions/authActions.js
@@ -1,1 +1,40 @@
-export const checkAuth = () => {};
+import * as actionTypes from "./actionTypes";
+
+export const logoutAction = (dispatch, getState) => {
+    dispatch({ type: actionTypes.LOGOUT });
+};
+
+export const loginAction = (username, password) => {
+    return (dispatch) => {};
+    // dispatch {
+    //     username: "",
+    //     token: undefined,
+    //     expirationDate: undefined,
+    // };
+};
+
+const getBoundedFunction = (dispatch, action) => {
+    return () => {
+        dispatch(action);
+    };
+};
+
+const setLogOutTimer = (timeGiven) => {
+    return (dispatch) => {
+        setTimeout(getBoundedFunction(dispatch, logoutAction), timeGiven);
+    };
+};
+
+export const checkAuthAction = (dispatch, getState) => {
+    const state = getState();
+    const currentUser = state.currentUser;
+    const token = currentUser.token;
+    if (token !== undefined) {
+        const expirationDate = new Date(localStorage.getItem("expirationDate"));
+        if (expirationDate <= new Date()) {
+            dispatch(logoutAction);
+        } else {
+            dispatch(setLogOutTimer(new Date() - expirationDate));
+        }
+    }
+};

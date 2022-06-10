5092b06dab5d49e9042e26c8e4c21214748a9c74
JANG <kennethjang34@gmail.com>
2022-06-10 11:26:38 -0700

    test

--- communication/models.py
+++ communication/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
+from django.db.models import constraints
 User = get_user_model()
 
-
+####
 def pkgen():
     from base64 import b32encode
     from hashlib import sha1
@@ -23,18 +23,44 @@ class Account(models.Model):
     userID = models.CharField(max_length=15, primary_key=True, default=pkgen)
     user = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
     following = models.ManyToManyField(
-        "Account",
-        related_name="followers",
-    )
+            "Account",
+            related_name="followers",
+            )
     timestamp = models.DateTimeField(auto_now_add=True)
     channel_name = models.CharField(max_length=100, default="")
 
-    def __str__(self):
-        return self.userID
-
-    # called whenever there is a new user
-    @receiver(post_save, sender=get_user_model())
-    def create_account(sender, instance, created, **kwargs):
-        if created:
-            account = Account.objects.create(userID=instance.username, user=instance)
+    def __str__(self): return self.userID # called whenever there is a new user 
+    @receiver(post_save, sender=get_user_model()) 
+    def create_account(sender, instance, created, **kwargs): 
+        if created: account = Account.objects.create(userID=instance.username, user=instance)
             # no friends !
+
+
+
+class FriendRequest(models.Model):
+    requester = models.ForeignKey(Account, related_name="friend_requests_sent", on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Account, related_name="friend_requests_received", on_delete=models.CASCADE)
+    accepted = models.BooleanField(blank=True, null=True, default=None)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Requester: " + self.requester.__str__() + ", Receiver: " + self.receiver.__str__()
+
+    
+    class Meta:
+        constraints = [
+                constraints.UniqueConstraint(
+                    fields=['requester', 'receiver'], name="unique_friendship_reverse"
+                    ),
+                models.CheckConstraint(
+                    name="prevent_self_follow",
+                    check=~models.Q(requester=models.F("receiver")),
+                    )
+                ]
+
+
+        #  db_table = 'FriendRequest'
+     #   constraints = [
+    #        models.UniqueConstraint(fields=['app_uuid', 'version_code'], name='unique appversion')
+   #     ]
+
